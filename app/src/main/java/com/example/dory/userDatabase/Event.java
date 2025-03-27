203fc70ab9f5daaf091b5b3236b133c121c0c56e
300391317 <tellezarriagag@student.douglascollege.ca>
2025-03-27 14:27:13 -0700

    Added Event Class with constructor, getter and setters

---
+++ app/src/main/java/com/example/dory/userDatabase/Event.java
@@ -0,0 +1,87 @@
+package com.example.dory.userDatabase;
+
+public class Event {
+    public int eventID;
+    public int organizerID;
+    public String title;
+    public String description;
+    public String startDate;
+    public String endDate;
+    public String location;
+    public int capacity;
+
+    public Event(int eventID, int organizerID, String title, String description, String startDate, String endDate, String location, int capacity) {
+        this.eventID = eventID;
+        this.organizerID = organizerID;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.capacity = capacity;
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public int getOrganizerID() {
+        return organizerID;
+    }
+
+    public void setOrganizerID(int organizerID) {
+        this.organizerID = organizerID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
